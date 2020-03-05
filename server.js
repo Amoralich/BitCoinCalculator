db0f0d4f49d4fde65298a533ac4aeb15c7c64773
Opilane TTHK <opilane@tthk.ee>
2020-03-05 13:04:45 +0200

    m

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const request = require("request")
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-app.get("/",function(req,res){
-    res.sendFile(__dirname+"/index.html")
+app.get('/', function(req,res){
+    res.sendFile(__dirname+'/index.html');
 });
+app.post('/', function(req,res){
 
-app.post("/", function(req, res){
+    let amount = req.body.amount;
     let currency = req.body.currency;
-    
-    request(`https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`, function(error, response, body){
-    console.log("Status message: ", response.statusMessage);
-    console.log("Server Status Code: ", response.statusCode);
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`
 
-    console.log(response.body);
+    request(url, function(error,response,body){
+        console.log("Status Message: ", response.statusMessage);
+        console.log("Server Status Code: ", response.statusCode);
+        console.log(response.body);
 
-    let data = JSON.parse(response.body);
-    let price;
+        let data = JSON.parse(response.body);
+        let price;
 
-    if(currency === "EUR") {
-        price = data.bpi.EUR.rate;
-        console.log("Price in EUR ", price);
-    }else{
-        price = data.bpi.EUR.rate;
-        console.log("Price in USD ", price);
-    }
+        if (currency === "EUR"){
+            price = data.bpi.EUR.rate_float;
+            console.log("Price in EUR", price);5
+        } else {
+            price = data.bpi.USD.rate_float;
+            console.log("Price in USD", price);
+        }
 
-    let diclaimer = data.disclaimer;
+        let disclaimer = data.disclaimer;
 
-    res.write(`${diclaimer}`);
-    res.write('<br>');
-    res.write(`Current prise in ${currency} is ${price}`);
-    res.send();
+        //price = price.replace(",","")
+        price = parseFloat(price) * parseFloat(amount)
 
-    });
+        res.write(`${disclaimer}`);
+        res.write(`<br>`);
+        res.write(`The price of ${amount} BitCoin(s) is ${price} ${currency}`)
+        res.send();
+    })
 });
 
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-}); 	
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running away.");
+});
